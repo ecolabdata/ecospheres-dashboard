e30fa8fae54cb9abf742d0aa997f190283ff52db
Alexandre Bulté <alexandre@bulte.net>
2024-08-02 12:08:51 +0200

    tweak config

--- superset_config.py
+++ superset_config.py
@@ -3,5 +3,10 @@ import os
 SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
 SECRET_KEY = "Ccdivw2Cmz53oUtXGPromUcEeGKkXM8H"
 PUBLIC_ROLE_LIKE = "Gamma"
-HTTP_HEADERS = {'X-Frame-Options': 'ALLOWALL'}
-OVERRIDE_HTTP_HEADERS = {'X-Frame-Options': 'ALLOWALL'}
+
+# does not work
+# HTTP_HEADERS = {'X-Frame-Options': 'ALLOWALL'}
+# OVERRIDE_HTTP_HEADERS = {'X-Frame-Options': 'ALLOWALL'}
+
+# disable talisman to disable restrictive X-Frame-Options
+TALISMAN_ENABLED = False
