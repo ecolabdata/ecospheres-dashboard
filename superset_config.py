8bf9e763cdb3557ee66cba6f5697ce65f7ed7ddd
Alexandre Bulté <alexandre@bulte.net>
2024-09-25 08:11:20 +0200

    add redis cache and pillow install

--- superset_config.py
+++ superset_config.py
@@ -2,10 +2,22 @@ import os
 
 SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
 SECRET_KEY = os.getenv("SECRET_KEY")
+REDIS_URL = os.getenv("REDIS_URL")
+
+if not SQLALCHEMY_DATABASE_URI or not SECRET_KEY or not REDIS_URL:
+    raise Exception("DATABASE_URL, SECRET_KEY and REDIS_URL must be set in the environment")
+
+# https://superset.apache.org/docs/security/#public
 PUBLIC_ROLE_LIKE = "Gamma"
 
-if not SQLALCHEMY_DATABASE_URI or not SECRET_KEY:
-    raise Exception("DATABASE_URL and SECRET_KEY must be set in the environment")
+# add caching like in https://github.com/apache/superset/blob/master/docker/pythonpath_dev/superset_config.py
+CACHE_CONFIG = {
+    "CACHE_TYPE": "RedisCache",
+    "CACHE_DEFAULT_TIMEOUT": 300,
+    "CACHE_KEY_PREFIX": "superset_",
+    "CACHE_REDIS_URL": REDIS_URL,
+}
+DATA_CACHE_CONFIG = CACHE_CONFIG
 
 # does not work
 # HTTP_HEADERS = {'X-Frame-Options': 'ALLOWALL'}
