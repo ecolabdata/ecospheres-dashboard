7a81cd823f8ad09020596217de26bdf633c67c16
Alexandre Bulté <alexandre@bulte.net>
2026-02-12 15:39:10 +0100

    fix: remove PUBLIC_ROLE_LIKE

--- superset_config.py
+++ superset_config.py
@@ -10,9 +10,6 @@ REDIS_URL = os.getenv("REDIS_URL")
 if not SQLALCHEMY_DATABASE_URI or not SECRET_KEY or not REDIS_URL:
     raise Exception("DATABASE_URL, SECRET_KEY and REDIS_URL must be set in the environment")
 
-# https://superset.apache.org/docs/security/#public
-PUBLIC_ROLE_LIKE = "Gamma"
-
 # add caching like in https://github.com/apache/superset/blob/master/docker/pythonpath_dev/superset_config.py
 CACHE_CONFIG = {
     "CACHE_TYPE": "RedisCache",
